d28463850061a79ce0de076eb2281239c9860a0f
Dabani <robertmaku@yahoo.fr>
2022-10-02 20:29:40 +0200

    ChangeColor method

--- Assets/Scripts/GroundPiece.cs
+++ Assets/Scripts/GroundPiece.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class GroundPiece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool isColored;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ChangeColor(Color color) {
+        GetComponent<MeshRenderer>().material.color = color;
+        isColored = true;
     }
 }
